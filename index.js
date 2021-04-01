dc37d5ba6e53f51e119b3317e6f10cc8c9c6b6a8
lizburkechi <liz.burke.chi@gmail.com>
2021-04-01 12:10:16 -0500

    commit tree e6c8b329a469c184af88c770e39d5b0c7b1f6930

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // Code your solutions in this file
-function writeCards( namesArray, event ) {
-    let thankYouCards = []
-    for ( let i = 0; i < namesArray.length; i++ ) {
-      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
-    }
-    return thankYouCards
+function writeCards(names, event) {
+  let thankYouCards = []
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
   }
-  
-  function countDown( startingNumber ) {
-    while ( startingNumber > 0 ) {
-      console.log( startingNumber );
-      startingNumber -= 1;
-    }
-    console.log( startingNumber );
-  }
+  return thankYouCards
+}
+
+function countDown( positiveInteger ) {
+while (positiveInteger > 0) {
+  console.log(positiveInteger);
+  positiveInteger--; 
+}
+console.log(positiveInteger);
+}
